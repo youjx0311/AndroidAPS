97d3455525d62a9d110d379ece29af17c8bd13da
小游 <132682034+youjx0311@users.noreply.github.com>
2024-06-22 19:01:27 +0800

    更新 XdripSourcePlugin.kt

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
@@ -49,9 +49,6 @@ class XdripSourcePlugin @Inject constructor(
     aapsLogger, rh, injector
 ), BgSource, XDripSource {
 
-    private var advancedFiltering = false
-    override var sensorBatteryLevel = -1
-
     override fun advancedFilteringSupported(): Boolean = true
 
     private fun detectSource(glucoseValue: GlucoseValue) {
