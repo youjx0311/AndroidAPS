80f46ddf03673479c9e7694c42d259a1830a3a90
小游 <132682034+youjx0311@users.noreply.github.com>
2025-11-01 02:42:19 +0800

    Update Event.kt

--- core/interfaces/src/main/kotlin/app/aaps/core/interfaces/rx/events/Event.kt
+++ core/interfaces/src/main/kotlin/app/aaps/core/interfaces/rx/events/Event.kt
@@ -1,13 +1,13 @@
-// core/interfaces/src/main/kotlin/app/aaps/core/interfaces/rx/events/Event.kt
 package app.aaps.core.interfaces.rx.events
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder
 import org.apache.commons.lang3.builder.ToStringStyle
 
-/** 事件总线的所有事件的基类 */
+/** Base class for all events posted on the event bus.  */
 abstract class Event {
+
     override fun toString(): String {
-        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE)
+        return ReflectionToStringBuilder.toString(this)
     }
 
     companion object {
