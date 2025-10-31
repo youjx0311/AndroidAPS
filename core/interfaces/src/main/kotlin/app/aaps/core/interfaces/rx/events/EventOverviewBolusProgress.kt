3d745fbfce594fd70c7955f63a5b2e7b1bd34df3
小游 <132682034+youjx0311@users.noreply.github.com>
2025-10-31 22:24:15 +0800

    Add Status enum to EventOverviewBolusProgress

--- core/interfaces/src/main/kotlin/app/aaps/core/interfaces/rx/events/EventOverviewBolusProgress.kt
+++ core/interfaces/src/main/kotlin/app/aaps/core/interfaces/rx/events/EventOverviewBolusProgress.kt
@@ -4,6 +4,12 @@ object EventOverviewBolusProgress : Event() {
 
     data class Treatment(var insulin: Double = 0.0, var carbs: Int = 0, var isSMB: Boolean, var id: Long)
 
+    enum class Status {
+        ATTEMPTING,
+        COMPLETED,
+        STOPPED
+    }
+
     var status = ""
     var t: Treatment? = null
     var percent = 0
